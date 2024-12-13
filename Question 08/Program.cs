b04ddd886b97b62cc71c41c0a986fcb0ba2a373c
adelian14 <aliadel922@gmail.com>
2024-12-13 17:07:34 +0200

    Question 8 commit

---
+++ Question 08/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Question_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i+=2)
+            {
+                if(i > 2)
+                    Console.Write(", ");
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
