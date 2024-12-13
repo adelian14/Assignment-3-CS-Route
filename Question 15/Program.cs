a4a9582229bd6f9aadd61d16037590349d1c2237
adelian14 <aliadel922@gmail.com>
2024-12-13 18:20:14 +0200

    Question 15 commit

---
+++ Question 15/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Question_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+            Console.Write("Input starting number of range: ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("Input ending number of range: ");
+            end = int.Parse(Console.ReadLine());
+            Console.WriteLine("The prime number between 1 and 50 are:");
+            for(int n = start; n <= end; n++)
+            {
+                bool isPrime = true;
+                for(int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime && n!=1)
+                {
+                    Console.Write($"{n} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
