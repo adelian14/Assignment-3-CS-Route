d1510f83ed5777e14d0cc9d8ffb848beb1776141
adelian14 <aliadel922@gmail.com>
2024-12-13 17:03:56 +0200

    Question 7 commit

---
+++ Question 07/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Question_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n*i}");
+            }
+        }
+    }
+}
