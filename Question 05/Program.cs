a3eda6b4dbe2b883496731f1a58e6e1501a64741
adelian14 <aliadel922@gmail.com>
2024-12-13 16:49:31 +0200

    Question 5 commit

---
+++ Question 05/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Question_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            Console.Write("Enter a character: ");
+            c = (char)Console.Read();
+            string vowels = "aeiouAEIOU";
+            bool isVowel = false;
+            for(int i = 0; i < vowels.Length; i++){
+                if (c == vowels[i]){
+                    isVowel = true;
+                    break;
+                }
+            }
+            if (isVowel){
+                Console.WriteLine($"'{c}' is a vowel");
+            }
+            else{
+                Console.WriteLine($"'{c}' is a consonant");
+            }
+        }
+    }
+}
