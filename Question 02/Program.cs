c355f56a8358384322919366613fc54d8adb62c5
adelian14 <aliadel922@gmail.com>
2024-12-13 16:20:02 +0200

    Question 2 commit

---
+++ Question 02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Question_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+            else
+            {
+                Console.WriteLine("Positive");
+            }
+        }
+    }
+}
