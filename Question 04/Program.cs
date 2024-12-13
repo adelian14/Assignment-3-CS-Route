73df35674b7b0b62ea1973f05bc0ef002a242824
adelian14 <aliadel922@gmail.com>
2024-12-13 16:41:44 +0200

    Question 4 commit

---
+++ Question 04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Question_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n%2 == 0){
+                Console.WriteLine($"{n} is even");
+            }
+            else{
+                Console.WriteLine($"{n} is odd");
+            }
+        }
+    }
+}
