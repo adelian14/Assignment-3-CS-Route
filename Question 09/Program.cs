04a3d076f776aa174071ab2285f5aeb2008b04e0
adelian14 <aliadel922@gmail.com>
2024-12-13 17:12:25 +0200

    Question 9 commit

---
+++ Question 09/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Question_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, p, ans = 1;
+            Console.Write("Enter bsae: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter power: ");
+            p = int.Parse(Console.ReadLine());
+            for(int i = 0; i < p; i++)
+            {
+                ans *= x;
+            }
+            Console.WriteLine($"{x} ^ {p} = {ans}");
+        }
+    }
+}
