df3451aee56d5c160a3a4329acc9027afacbfdbb
adelian14 <aliadel922@gmail.com>
2024-12-13 18:26:05 +0200

    Question 16 commit

---
+++ Question 16/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Question_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to convert: ");
+            int x = int.Parse(Console.ReadLine());
+            int n = x;
+            string s = "";
+            do
+            {
+                if (x % 2 == 0)
+                {
+                    s = "0" + s;
+                }
+                else
+                {
+                    s = "1" + s;
+                }
+                x /= 2;
+            } while (x > 0);
+            Console.WriteLine($"The Binary of {n} is {s}");
+        }
+    }
+}
