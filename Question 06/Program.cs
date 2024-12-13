bf5fd64c44018533bb63cb50df4be6afc21312ad
adelian14 <aliadel922@gmail.com>
2024-12-13 16:59:45 +0200

    Question 6 commit

---
+++ Question 06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Question_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write(i);
+                if (i < n)
+                    Console.Write(", ");
+                else
+                    Console.WriteLine();
+            }
+
+        }
+    }
+}
