df8dfa5facfee71d117eb6cada6c01eb0a2bea8a
adelian14 <aliadel922@gmail.com>
2024-12-13 18:12:54 +0200

    Question 14 commit

---
+++ Question 14/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Question_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 0;
+            Console.Write("Enter a number: ");
+            x = int.Parse(Console.ReadLine());
+            while(x > 0)
+            {
+                y = y * 10 + x % 10;
+                x /= 10;
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
