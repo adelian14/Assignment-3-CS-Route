ca7f7c07a4b2eb58c4277c0f3073dd8a65a36dc8
adelian14 <aliadel922@gmail.com>
2024-12-13 18:10:17 +0200

    Question 13 commit

---
+++ Question 13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Question_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, t = "";
+            Console.Write("Enter a string: ");
+            s = Console.ReadLine();
+            for(int i = s.Length-1; i > -1; i--)
+            {
+                t += s[i];
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
